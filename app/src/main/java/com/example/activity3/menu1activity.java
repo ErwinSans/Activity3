9923e02cf3b87e28ed6b16e38e991845fa044f3f
Erwin Santosa <erwincoc232@gmail.com>
2020-03-30 13:54:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity3/menu1activity.java
@@ -0,0 +1,20 @@
+package com.example.activity3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.activity3.R;
+
+public class menu1activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1activity);
+    }
+}
---
+++ app/src/main/java/com/example/activity3/menu2Activity.java
@@ -0,0 +1,16 @@
+package com.example.activity3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.activity3.R;
+
+public class menu2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu2);
+    }
+}
+
